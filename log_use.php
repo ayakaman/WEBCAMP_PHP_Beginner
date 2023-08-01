5e861c5f9020171c35d60146e8b93e01f71cd2c2
ayaka <ayakaman@example.com>
2023-08-01 01:49:47 +0000

    WEBCAMP_Log.php log_use.php log_use_2.phpを書いた

---
+++ log_use.php
@@ -0,0 +1,8 @@
+<?php
+
+//ファイル読み込み
+require_once( __DIR__ . "/WEBCAMP_Log.php" );
+
+//名前空間付き　のクラス使用
+$obj = new \WEBCAMP\Log();
+$obj->test();
---
+++ log_use_2.php
@@ -0,0 +1,10 @@
+<?php
+
+//ファイル読み込み
+require_once( __DIR__ . "/WEBCAMP_Log.php" );
+
+use WEBCAMP\Log;
+
+//名前空間付き　のクラス使用
+$obj = new \WEBCAMP\Log();
+$obj->test();
